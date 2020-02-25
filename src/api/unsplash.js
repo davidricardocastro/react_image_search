825f30178b8bdb2d59d6af197363814b54fc32d4
David Castro <31487462+davidricardocastro@users.noreply.github.com>
2020-02-25 15:08:07 +0200

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID a4bb014e31d5e864b46c6b0f7242abdc87c954efd72f4424830f047b7a16ebe2'
+        Authorization: 'Client-ID HERE'
     }
 });
